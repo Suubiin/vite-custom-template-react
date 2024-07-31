6c170c04e01686b5d6de0bd12328a5b0a0248a64
Suubiin <suubiin0819@gmail.com>
2024-07-31 23:03:55 +0900

    [과제] vite 커스텀 템플릿

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import '@/styles/globals.css';
+import App from '@/App';
+
+// console.log(import.meta.env);
+
+const domNode = document.getElementById('react-app');
+createRoot(domNode).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
